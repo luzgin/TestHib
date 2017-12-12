5a26ac01bb8897a3e931d6d58b88d2c197b7339e
egor.luzgin <lew.vt1@gmail.com>
2017-12-12 16:37:11 +0300

    add first record in mysql

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import entity.Student;
+import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.metadata.ClassMetadata;
 import org.hibernate.service.ServiceRegistry;
@@ -12,8 +10,17 @@ import java.util.Map;
 public class Main {
         public static void main(final String[] args) {
             SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-
             Session session = sessionFactory.openSession();
+
+            Transaction transaction = session.beginTransaction();
+
+            Student student = new Student();
+            student.setFirstName("Ivan");
+            student.setLastName("Ivanov");
+            student.setAge(25);
+
+            session.save(student);
+            transaction.commit();
 
             session.close();
             sessionFactory.close();
